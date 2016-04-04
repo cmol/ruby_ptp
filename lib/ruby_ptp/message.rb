693d3679575b9d6c89c025fbac6a45c729ecfd63
Claus Lensbøl <cmol@cmol.dk>
2016-04-04 11:15:41 +0200

    Early packet creation

--- lib/ruby_ptp/message.rb
+++ lib/ruby_ptp/message.rb
@@ -20,10 +20,14 @@ module RubyPtp
     MANAGEMENT            = 0xd
 
     def initialize options = {}
-      if options && options[:parse]
+      if options[:parse]
         parse(options[:parse])
       else
-        # Are we making a new packet?
+        # We are making a new packet
+        @flagField = 0x0000
+        @versionPTP = 0x2
+        @domainNumber = 0x0
+        @sourcePortIdentityPortNumer = 0x1
       end
     end
 
@@ -33,14 +37,14 @@ module RubyPtp
 
       # Firstly unpack the header
       tmp_tran_type, @versionPTP, @messageLength, @domainNumber, res1,
-        @flagField, @correctionField, res2, spi1, spi2, @sequenceId,
-        @controlField, @logMessageInterval = header.unpack("CCSCCSQLQSSCC")
+        @flagField, @correctionField, res2, @sourcePortIdentity, portno,
+        @sequenceId, @controlField,
+        @logMessageInterval = header.unpack("CCS>CCS>Q>L>Q>S>S>CC")
 
       # Fixup fields of 4 bits or not in 2^n octets
       @transport = tmp_tran_type >> 4
       @messageType = tmp_tran_type & 0xf
-      res1 = nil; res2 = nil
-      @sourcePortIdentity = (spi1 << 64) | spi2
+      res1 = nil; res2 = nil; portno = nil
 
       # Parse the payload specifics of the message
       case @messageType
@@ -52,7 +56,7 @@ module RubyPtp
         sec = (sec1 << 16) | sec2
 
         # Check if the sending clock is a two-step
-        if sec == 0 && nsec == 0
+        if (@flagField & 1) == 1 || sec == 0 && nsec == 0
           @originTimestamp = -1
         else
           @originTimestamp = [sec, nsec]
@@ -84,5 +88,41 @@ module RubyPtp
         raise ArgumentError
       end
     end
+
+    # Construct packet for sending and return the string to send
+    def readyMessage(type)
+
+      # Create the packet payload
+      payload = nil
+      case type
+      when DELAY_REQ
+        payload = [0x0,0x0].pack("Q>S>")
+
+        # Set defaults for DELAY_REQ packets
+        @messageType        = DELAY_REQ
+        @controlField       = DELAY_REQ
+        @correctionField    = 0x0
+        @messageLength      = 0x2c
+        @logMessageInterval = 0x7f
+      end
+
+      # Make the header for the packet
+      header = [@messageType,                 # C
+                @versionPTP,                  # C
+                @messageLength,               # S>
+                @domainNumber,                # C
+                0x0,                          # C - reserved
+                @flagField,                   # S>
+                @correctionField,             # Q>
+                0x0,                          # L> - reserved
+                @sourcePortIdentity,          # Q>
+                @sourcePortIdentityPortNumer, # S>
+                @controlField,                # C
+                @logMessageInterval           # C
+        ].pack("CCS>CCS>Q>L>Q>S>CC")
+
+      return header + payload
+    end
+
   end
 end
