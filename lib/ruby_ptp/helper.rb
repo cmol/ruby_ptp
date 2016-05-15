44b2d37403a92a3eb10faee2d1a9fc7d5097f2b7
Claus Lensbøl <cmol@cmol.dk>
2016-05-16 00:29:12 +0200

    Ensure filewrite

--- lib/ruby_ptp/helper.rb
+++ lib/ruby_ptp/helper.rb
@@ -3,14 +3,16 @@ module RubyPtp
     def self.write_data(path: "./", files: nil)
       files.each do |file|
         File.open(path+file[:name]+".dat", 'w') do |f|
-          idx = 0
+          puts "Writing file #{file[:name]}"
+          idx = -1
           f.write("x\ty\n")
           file[:data].each do |d|
+            idx += 1
+            next unless d
             if file[:name] =~ /freq.*/
               d = (d - 1) * 1000000
             end
             f.write("#{idx}\t#{d.to_f}\n")
-            idx += 1
           end
         end
       end
